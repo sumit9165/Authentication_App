e4efccabcd39cbafb8ed7f31660d53df9f1b70da
sumit9165 <pawarsumit364@gmail.com>
2026-07-07 17:45:10 +0530

    Added: flake8 installed and linting fixed

--- seller/views.py
+++ seller/views.py
@@ -3,6 +3,8 @@ from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from core.mixins import IsSellerMixin
 # Create your views here.
+
+
 class SellerDashboardView(LoginRequiredMixin, View, IsSellerMixin):
     def get(self, request, *args, **kwargs):
         return render(request, 'seller/dashboard.html')
