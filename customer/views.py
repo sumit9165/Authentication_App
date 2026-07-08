5c621e2b916b13a0b5ac5e14a0a9ba29d1ec923e
sumit9165 <pawarsumit364@gmail.com>
2026-07-09 01:03:14 +0530

    Fixed: length of code

--- customer/views.py
+++ customer/views.py
@@ -16,7 +16,9 @@ class CustomerDashboardView(LoginRequiredMixin, View, IsCustomerMixin):
 
 
 # type: ignore
-class CustomerPasswordChangeView(PasswordChangeView, LoginRequiredMixin, IsCustomerMixin):
+class CustomerPasswordChangeView(
+    PasswordChangeView, LoginRequiredMixin, IsCustomerMixin
+    ):
     template_name = "customer/password_change.html"
     success_url = reverse_lazy("login")
 
@@ -25,13 +27,15 @@ class CustomerPasswordChangeView(PasswordChangeView, LoginRequiredMixin, IsCusto
         logout(self.request)
         messages.success(
             self.request,
-            "Password changed successfully. Please log in with your new password.",
+            "Password changed successfully." 
+            "Please log in with your new password.",
         )
         return response
 
     def form_invalid(self, form):
         messages.error(
             self.request,
-            "There was an error changing your password. Please try again",
+            "There was an error changing your password."
+            "Please try again.",
         )
         return super().form_invalid(form)
